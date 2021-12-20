8df032c5ec4765c2509e90adccace5b983db2915
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-20 09:21:01 +0700

    deen: Add custom boost extension for scroll handling
    Currently, we're not using CAF's BoostFramework due to having a pure
    AOSP based source, thus qcom perf hinting are not correctly applied.
    This extension will handle the vertical fling through device-specific
    boost override, utilizing existing libqti-perfd hinting.
    So any further configuration should be happens inside vertical scroll
    config inside perfboostsconfig.xml
    
    Change-Id: I823c9383041b68ff2ce62d5f1a1c5091b2dfac15

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,6 +165,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_POWERHAL_BOOST_EXT := $(DEVICE_PATH)/configs/power/boost-ext.cpp
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
