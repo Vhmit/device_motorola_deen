0f89a30f8a184bfbe19611b02af778baf66b30d8
Vhmit <viktor.9630@protonmail.com>
2025-11-27 16:58:40 -0300

    deen: Update perf stack for LA.UM.8.6.2.r1-08600-89xx.0
    [Vhmit]: Kang perf configs from stock deen QPKS30.54-22-27
    
    Co-authored-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- device.mk
+++ device.mk
@@ -336,8 +336,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power.stats@1.0-service.mock \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.0 \
-    vendor.qti.hardware.perf@2.0.vendor
+    vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.1.vendor
 
 # Powerhint
 PRODUCT_COPY_FILES += \
