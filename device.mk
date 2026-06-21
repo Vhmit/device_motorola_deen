5c4ac12812f9792f9b0a6d7738ab8a3660355623
Vhmit <viktor.9630@protonmail.com>
2026-06-21 12:25:35 -0300

    Revert "deen: Add GoogleCameraGoPrebuilt"
    This reverts commit 099b07d840a9a45d9615acb6d8b00f1c1533ac59.

--- device.mk
+++ device.mk
@@ -117,12 +117,7 @@ PRODUCT_PACKAGES += \
     libbson.vendor \
     libxml2 \
     libwui \
-    vendor.qti.hardware.camera.device@1.0:64 \
-    GoogleCameraGoPrebuilt
-
-# QTI Permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml     
+    vendor.qti.hardware.camera.device@1.0:64
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
