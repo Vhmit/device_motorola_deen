dbe691badd25da83168126e62989670c3de845c6
Vhmit <viktor.9630@protonmail.com>
2026-08-10 20:05:51 -0300

    deen: Downgrade GPS HAL to LA.UM.8.6.r1-02600-89xx.0
    - Newer HAL causes mismatch between blobs and the HAL
    - Delete the location HAL because it conflicts with the GPS.
    
    Revert this commit 143f3b0a2a57a27d07c28103eef2ed747df9c967.
    Revert this commit cc134d5b1a3e4272bbcb93ddeb869d98cf159b56.
    Revert this commit c790ecb7d4a1688ede4a5455338a9e7d771a7ffd.
    
    Co-authored-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- device.mk
+++ device.mk
@@ -160,14 +160,14 @@ PRODUCT_PACKAGES += \
     libsynergy_loc_api \
     libwifi-hal-ctrl
 
-PRODUCT_PACKAGES += \
-    apdr.conf \
-    flp.conf \
-    gps.conf \
-    izat.conf \
-    lowi.conf \
-    sap.conf \
-    xtwifi.conf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
+    $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
