05d2ee9373d2c1cddfaac922fe314200a1b029fc
Vhmit <viktor.9630@protonmail.com>
2026-09-06 15:54:04 -0300

    deen: Enable and build cryptfshw HIDL
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- device.mk
+++ device.mk
@@ -196,6 +196,10 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# HW crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # Init
 PRODUCT_PACKAGES += \
     init.class_main.sh \
