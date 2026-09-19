0ade96cb8ee44c6b4cb4732cd2d147f297455aed
Vhmit <viktor.9630@protonmail.com>
2026-09-19 19:51:53 -0300

    Revert "deen: Move over to AOSP way of shipping kernel"
    This reverts commit ca7da7a61f7e99f434902df7b774fffeab59c3fa.

--- device.mk
+++ device.mk
@@ -263,11 +263,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sysfs
 
-# Kernel
-LOCAL_KERNEL := device/motorola/deen-kernel/Image.gz-dtb
-PRODUCT_COPY_FILES += \
-	$(LOCAL_KERNEL):kernel
-
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/,$(TARGET_COPY_OUT_VENDOR)/etc)
